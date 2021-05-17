c04b0c8db27087ad7d7ea3ad08e952eafa8111b1
dhc1947 <82545241+dhc1947@users.noreply.github.com>
2021-05-17 17:10:48 +0200

    Add files via upload

--- popup.js
+++ popup.js
@@ -5,42 +5,50 @@ let helloWorldPopup = undefined;
 // Open the popup when we enter a given zone
 WA.onEnterZone('popup', () => {
 console.log('EnterPopup started successfully');
-    helloWorldPopup = WA.openPopup("popuprectangle", 'Willkommen im Pfingstlager 2021. Mal schauen, in welchem Team du bist...', [{
+    helloWorldPopup = WA.openPopup("popuprectangle", 'Willkommen im Pfingstlager 2021. Mal schauen, in welcher Gruppe du bist...', [{
         label: "Weiter",
         className: "primary",
         callback: (popup) => {
             // Close the popup when the "Close" button is pressed.
             popup.close();
-            helloWorldPopup = WA.openPopup("popuprectangle", 'Team 1: ', [{
+            helloWorldPopup = WA.openPopup("popuprectangle", 'Gruppe 1: Jan K., Judith K., Adrian W., Alex P.& Myriam E.', [{
                 label: "Weiter",
                 className: "primary",
                 callback: (popup) => {
                     // Close the popup when the "Close" button is pressed.
                     popup.close();
-                    helloWorldPopup = WA.openPopup("popuprectangle", 'Team 2: ', [{
+                    helloWorldPopup = WA.openPopup("popuprectangle", 'Gruppe 2: Theo T., Tobias K., Miko G., Mareike T. & Leonie S.', [{
                         label: "Weiter",
                         className: "primary",
                         callback: (popup) => {
                             // Close the popup when the "Close" button is pressed.
                             popup.close();
-                            helloWorldPopup = WA.openPopup("popuprectangle", 'Team 3:', [{
+                            helloWorldPopup = WA.openPopup("popuprectangle", 'Gruppe 3: Fiona U., Max L., Birger H., Joshua B. & Elli N.', [{
                                 label: "Weiter",
                                 className: "primary",
                                 callback: (popup) => {
                                     // Close the popup when the "Close" button is pressed.
                                     popup.close();
-                                    helloWorldPopup = WA.openPopup("popuprectangle", 'Team 4: ', [{
+                                    helloWorldPopup = WA.openPopup("popuprectangle", 'Gruppe 4: Ben B., Sebastian P., Jan L., Kira G. & Antonia B.', [{
                                         label: "Weiter",
                                         className: "primary",
                                         callback: (popup) => {
                                             // Close the popup when the "Close" button is pressed.
                                             popup.close();
-                                            helloWorldPopup = WA.openPopup("popuprectangle", 'Team 5: ', [{
-                                                label: "Schließen",
+                                            helloWorldPopup = WA.openPopup("popuprectangle", 'Gruppe 5: Justus H., Johannes T., Konstantin O., Johanna N. & Anna B.', [{
+                                                label: "Weiter",
                                                 className: "primary",
                                                 callback: (popup) => {
                                                     // Close the popup when the "Close" button is pressed.
                                                     popup.close();
+                                                    helloWorldPopup = WA.openPopup("popuprectangle", 'Gruppe 6: Mariella K., Samuel B., Lisa / Sarah K., Klea K. & Jörg U.', [{
+                                                        label: "Schließen",
+                                                        className: "primary",
+                                                        callback: (popup) => {
+                                                            // Close the popup when the "Close" button is pressed.
+                                                            popup.close();
+                                                        }
+                                                    }]);
                                                 }
                                             }]);
                                         }
